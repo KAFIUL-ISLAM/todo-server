c9840f6db4cf2acc51d78834389909ddc573ae61
Al Kafi <alkafi28@gmail.com>
2022-05-19 01:23:52 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,29 +1,85 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
 
 
-const uri = "mongodb+srv://todo-list:<password>@cluster0.mniiv.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mniiv.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
-
-
-
 
 
 app.get('/', (req, res) => {
     res.send('Running my server');
 })
+
+async function run() {
+    try {
+        // Connect to DB
+        await client.connect();
+        const taskCollection = client.db("todo-list_DB").collection("tasks");
+
+        // GET
+        app.get('/tasks', async (req, res) => {
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+        })
+
+        // GET single item
+        app.get('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await taskCollection.findOne(query);
+            res.send(item);
+        })
+
+        // POST
+        app.post('/tasks', async (req, res) => {
+            const newTask = req.body;
+            const result = await taskCollection.insertOne(newTask);
+            res.send(result);
+        })
+
+        //PUT
+        app.put('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedTask = req.body;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    state : updatedTask
+                }
+            }
+            const result = await taskCollection.updateOne(query, updatedDoc, options);
+            res.send(result);
+        })
+
+        // DELETE
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+
 app.listen(port, () => {
     console.log('Running is started');
 })

7dc4e4e1cdb6f1a5e1963e1d54e8d800f8947544
Al Kafi <alkafi28@gmail.com>
2022-05-19 01:52:01 +0600

    crud added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mniiv.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wgfzf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
